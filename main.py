4507e08871fa6855aac3d9b7656b17490a810bb4
PeDiot <p.emmanuel.diot@gmail.com>
2025-02-20 09:23:48 +0100

    wip

--- main.py
+++ main.py
@@ -152,10 +152,10 @@ def main() -> None:
         config.index = 0
         loader = get_data_loader(bq_client, config)
 
-    # if src.bigquery.update_job_index(bq_client, config.id, config.index + 1):
-    #     print(f"Updated job index for {config.id} to {config.index+1}.")
-    # else:
-    #     print(f"Failed to update job index for {config.id}.")
+    if src.bigquery.update_job_index(bq_client, config.id, config.index + 1):
+        print(f"Updated job index for {config.id} to {config.index+1}.")
+    else:
+        print(f"Failed to update job index for {config.id}.")
 
     item_ids, vinted_ids, pinecone_point_ids = [], [], []
     n = n_success = n_available = n_unavailable = n_updated = 0
